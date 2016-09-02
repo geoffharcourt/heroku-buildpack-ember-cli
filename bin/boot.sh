ebae538e3e4d12541c14789d43ef3ef8869d6279
Nicolo <nicolo.giorgi@gmail.com>
2016-09-02 18:02:04 -0400

    wip

--- bin/boot.sh
+++ bin/boot.sh
@@ -8,4 +8,5 @@ touch $HOME/logs/nginx/access.log $HOME/logs/nginx/error.log
 
 (tail -f -n 0 $HOME/logs/nginx/*.log &)
 
-exec $HOME/vendor/nginx/sbin/nginx -p $HOME -c $HOME/config/nginx.conf
+export LD_LIBRARY_PATH=$HOME/vendor/openresty-1.9.15.1/build/luajit-root/app/vendor/openresty-heroku-build/luajit/lib:$LD_LIBRARY_PATH
+exec $HOME/vendor/openresty-heroku-build/nginx/sbin/nginx -p $HOME -c $HOME/config/nginx.conf
